77c8238edf67e3a88b2102f54ab21f0545715ef8
Meirin <meirin.f@gmail.com>
2017-02-06 19:28:26 +0100

    Creado Objeto Reloj

---
+++ WpfApplication1/Reloj.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+   
+    public class Reloj 
+    {
+        /// getter y setter
+        public String data { get; set; }
+        //Constructor 
+        public Reloj(String data)
+        {
+            this.data = data;
+        }
+
+
+
+
+    }
+}
